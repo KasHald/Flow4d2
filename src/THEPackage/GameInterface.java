a3e27d758f344cf7a9e508454601200bd9f9c892
EsbenKaas <esben.kaas@hotmail.com>
2013-11-25 12:43:55 +0100

    0.4.3

--- src/THEPackage/GameInterface.java
+++ src/THEPackage/GameInterface.java
@@ -4,11 +4,43 @@
  */
 package THEPackage;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Esben
  */
 public interface GameInterface
 {
+    String getQuizName();
     
+    String getQuestionButtonText();
+    
+    String getAnswerButtonText();
+    
+    String getLookupButtonText();
+    
+    String getQuestionLabelText();
+    
+    String getAnswerLabelText();
+    
+    ArrayList<WordPair> getPairs();
+    
+    String getAnswer();
+    
+    String getQuestion();
+    
+    String getRandomQuestion();
+    
+    int size();
+    
+    boolean checkGuess(String question, String guess);
+    
+    String Lookup(String question);
+    
+            
+           
+
+    
+   
 }
