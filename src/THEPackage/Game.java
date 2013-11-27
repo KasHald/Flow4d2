afb76265c86b321469b47d59bafa9abde670392f
Kasper Møller Hald <kasper@17b.dk>
2013-11-27 08:58:54 +0100

    0.4.5

--- src/THEPackage/Game.java
+++ src/THEPackage/Game.java
@@ -3,7 +3,7 @@ package THEPackage;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Game {
+public class Game implements GameInterface {
     
     private ArrayList<WordPair> pairs;
     private String quizName, questionButtonText, answerButtonText, lookupButtonText, questionLabelText, answerLabelText;
@@ -19,48 +19,86 @@ public class Game {
         this.r = new Random();
     }
     
+    @Override
     public String getQuizName(){
         return this.quizName;
     }
     
+    @Override
     public String getQuestionButtonText(){
         return this.questionButtonText;
     }
     
+    @Override
     public String getAnswerButtonText(){
         return this.answerButtonText;
     }
     
+    @Override
     public String getLookupButtonText(){
         return this.lookupButtonText;
     }
     
+    @Override
     public String getQuestionLabelText(){
         return this.questionLabelText;
     }
     
+    @Override
     public String getAnswerLabelText(){
         return this.answerLabelText;
     }
     
+    @Override
     public ArrayList<WordPair> getPairs(){
         return pairs;
     }
     
+    @Override
     public String getAnswer(int i){
         return pairs.get(i).getAnswer();
     }
     
+    @Override
     public String getQuestion(int i){
         return pairs.get(i).getQuestion();
     }
     
+    @Override
     public String getRandomQuestion(){
         return getQuestion(r.nextInt(pairs.size()));
     }
     
+    @Override
     public void addWordPair(WordPair wp){
         pairs.add(wp);
+    }
+
+    @Override
+    public int size() {
+        return pairs.size();
+    }
+
+    @Override
+    public boolean checkGuess(String question, String guess) {
+        for (int x = 0; x < pairs.size(); x++){
+            if (pairs.get(x).getQuestion().equalsIgnoreCase(question)){
+                if (pairs.get(x).getAnswer().equalsIgnoreCase(guess)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String Lookup(String question) {
+        for (int x = 0; x < pairs.size(); x++){
+            if (pairs.get(x).getQuestion().equalsIgnoreCase(question)){
+                return pairs.get(x).getAnswer();
+            }
+        }
+        return null;
     }
     
     
--- src/THEPackage/GameInterface.java
+++ src/THEPackage/GameInterface.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package THEPackage;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Esben
- */
 public interface GameInterface
 {
     String getQuizName();
@@ -26,21 +18,17 @@ public interface GameInterface
     
     ArrayList<WordPair> getPairs();
     
-    String getAnswer();
+    String getAnswer(int i);
     
-    String getQuestion();
+    String getQuestion(int i);
     
     String getRandomQuestion();
+    
+    void addWordPair(WordPair WP);
     
     int size();
     
     boolean checkGuess(String question, String guess);
     
     String Lookup(String question);
-    
-            
-           
-
-    
-   
 }
